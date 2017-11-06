0042ceac9d2b8f37094666b57cc5a99ca402f04f
guldtrollet <guldtrollet88@gmail.com>
2017-11-06 09:57:04 +0100

    Moved to css
    flyttat saker till css

--- src/js/classes/LoginPage.js
+++ src/js/classes/LoginPage.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import '../../css/LoginPage.css';
 
 class LoginPage extends Component {
   render() {
-const topbar = {
+
+/*const topbar = {
             backgroundColor: 'red',
             position: 'absolute',
             width: '100%',
@@ -12,8 +14,8 @@ const topbar = {
             fontSize: '40px',
             textIndent: '10%'
         };
-
-        const leftlogin = {
+*/
+       const leftlogin = {
             backgroundColor: 'pink',
             position: 'absolute',
             left: '10%',
@@ -30,7 +32,7 @@ const topbar = {
             width: '37%',
             height: '80%',
         };
-
+/* 
         const substyle = {
             position: 'absolute',
             bottom: '5%',
@@ -41,25 +43,32 @@ const topbar = {
             position: 'absolute',
             top: '10%',
             margin: 'center'
-        };
+        };*/
 
         
 
 		return(
 			<div style={{position: 'absolute', width: '100%', height: '90%', top: '10%'}}>
-              	<div style={topbar}>
+              	<div className = 'topbar'>
                     Bookface
                	</div>
+
+               {/* <div className='leftlogin'>
+                    left
+                </div>*/}
 
                 <div style={leftlogin}>
                    	left
                 </div>
+
+                {/*<div className='rightlogin'>*/}
+
                 <div style={rightlogin}>
                     right
 					<div style={{padding: '20%'}}>
                         <form style={{ margin:'0 auto',textalign:'center'}}>
                             <label>
-                                <input type="text" name="name" style={namestyle}/>
+                                <input type="text" name="name" className='namestyle'/>
                             </label>
 
                             <div>
@@ -71,7 +80,7 @@ const topbar = {
                                 <hr/>
                             </div>
 
-							<input type="submit" value="Submit" style={substyle} />
+							<input type="submit" value="Submit" className='substyle' />
                         </form>
                     </div>           
                 </div>
