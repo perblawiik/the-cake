6a3d424ffcee7d48bed0c6b7e0f72f90f248604c
Per Blåwiik <perj.sg@gmail.com>
2017-11-29 12:26:54 +0100

    GameOver css

--- src/js/classes/BrowserField.js
+++ src/js/classes/BrowserField.js
@@ -5,24 +5,16 @@ import '../../css/BrowserField.css';
 
 class BrowserField extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			setGameState: this.props.setGameState
-		}
-	}
-
 	render() {
 
 		const browserText = "https://www.bookface.com/" + this.props.pageInfo;
 
-		const containerStyle = {
+		const fieldContainer = {
 			position: 'absolute', width: '100%', height: '10%', backgroundColor: '#dddddd'
 		};
 
 		return (
-			<div style={containerStyle}>
-				<img src={require('../../img/left-arrow.png')} alt='x' className='arrowStyle' onClick={this.state.setGameState.bind(this, this.props.otherGameState)}/>
+			<div style={fieldContainer}>
 				<img src={require('../../img/browser.png')} alt='x' className='imageStyle'/>
 				<div className='textFieldStyle'>
 					<p>{browserText}</p>
