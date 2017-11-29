8a2d1b39423ee2a211b1b1d3170f51337b8c64b7
Per Blåwiik <perj.sg@gmail.com>
2017-11-29 16:00:16 +0100

    Tutorial half finished

--- src/js/classes/Tutorial.js
+++ src/js/classes/Tutorial.js
@@ -2,26 +2,89 @@ import React, { Component } from 'react';
 
 import '../../css/Tutorial.css';
 
+const Page = {
+    FIRST: 1,
+    SECOND: 2,
+    THIRD: 3,
+    FOURTH: 4
+}
+
 class Tutorial extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: Page.FIRST
+        }
+    }
+
+    completeTutorial() {
+        this.props.setGameState(this.props.mainPage)
+    }
+
+    setPage(p) {
+        this.setState({
+            page: p
+        });
+    }
 
     render() {
 
-        return (
-            <div className= 'tutorialDiv'>
-                <div className = 'anonymousPicture'>
-                    <p style={{marginTop: '20%', fontSize: '76px', textAlign: 'center'}}>
-                        PICTURE
-                    </p>
-                    <div className = 'youKnowStuff'>
-                        <p style={{marginTop: '20%', fontSize: '76px', textAlign: 'center'}}>
-                            GAME OVER LOSER !
-                        </p>
+        switch(this.state.page) {
+
+            case Page.FIRST:
+                return(
+                    <div className= 'tutorialDiv'>
+                        <div className = 'anonymousPicture' id='first'>
+                            <div className='navigationBar'>
+                            <button className='pageButton' onClick={this.completeTutorial.bind(this)}>SKIP</button>
+                            <button className='pageButton' onClick={this.setPage.bind(this, Page.SECOND)}>NEXT</button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-            </div>
-
-        );
+                );
+            case Page.SECOND:
+                return (
+                    <div className= 'tutorialDiv'>
+                        <div className = 'anonymousPicture' id='second'>
+                            <div className='navigationBar'>
+                                <button className='pageButton' onClick={this.setPage.bind(this, Page.FIRST)}>PREVIOUS</button>
+                                <button className='pageButton' onClick={this.setPage.bind(this, Page.THIRD)}>NEXT</button>
+                            </div>
+                        </div>
+                    </div>
+                );
+            case Page.THIRD:
+                return(
+                    <div className= 'tutorialDiv'>
+                        <div className = 'anonymousPicture' id='third'>
+                            <div className='navigationBar'>
+                                <button className='pageButton' onClick={this.setPage.bind(this, Page.SECOND)}>PREVIOUS</button>
+                                <button className='pageButton' onClick={this.setPage.bind(this, Page.FOURTH)}>NEXT</button>
+                            </div>
+                            <p style={{fontSize: '30px', textAlign: 'center'}}>
+                                Trollbar level stuff, Community Status stuff
+                            </p>
+                        </div>
+                    </div>
+                );
+            case Page.FOURTH:
+                return(
+                    <div className= 'tutorialDiv'>
+                        <div className = 'anonymousPicture'  id='fourth'>
+                            <div className='navigationBar'>
+                                <button className='pageButton' onClick={this.setPage.bind(this, Page.THIRD)}>PREVIOUS</button>
+                                <button className='pageButton' onClick={this.completeTutorial.bind(this)}>FINISH</button>
+                            </div>
+                            <p style={{fontSize: '30px', textAlign: 'center'}}>
+                                Good luck!
+                            </p>
+                        </div>
+                    </div>
+                );
+            default:
+                break;
+        }
     }
 }
 
